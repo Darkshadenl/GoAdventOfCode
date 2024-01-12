d2ff5ce3af1552de601a962eb0a7181f0ef7993a
Quinten M <quinten@infi.nl>
2024-01-12 18:32:51 +0100

    progress

--- main.go
+++ main.go
@@ -7,7 +7,22 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 )
+
+var writtenNumbers = map[string]int{
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+}
+var startWrittenNumbers = []string{"o", "t", "f", "s", "e", "n"}
+var endWrittenNumbers = []string{"e", "o", "r", "x", "n", "t"}
 
 func main() {
 	data, err := os.Open("calibrationcodes.txt")
@@ -30,6 +45,7 @@ func findCalibrationValues(r io.Reader) int {
 	for scanner.Scan() {
 		code := scanner.Text()
 		log.Println(code)
+
 		firstNumber := scanForNumber(code, false)
 		lastNumber := scanForNumber(code, true)
 		combinedStr := fmt.Sprintf("%d%d", firstNumber, lastNumber)
@@ -52,6 +68,12 @@ func findCalibrationValues(r io.Reader) int {
 }
 
 func scanForNumber(code string, reverse bool) int {
+	// use writtenNumbers. Replace all occurences of writtenNumbers with numbers
+
+	for k, v := range writtenNumbers {
+		code = strings.ReplaceAll(code, k, strconv.Itoa(v))
+	}
+
 	if reverse {
 		for i := len(code) - 1; i > -1; i-- {
 			isNumber, s := isNumber(code[i])
@@ -69,6 +91,35 @@ func scanForNumber(code string, reverse bool) int {
 	}
 	return -1
 }
+
+//func isWrittenNumber(code string, reverse bool) bool {
+//	for i := 0; i < len(code); i++ {
+//		letter := code[i]
+//		if reverse {
+//			// check if letter is in endWrittenNumbers
+//			for _, v := range endWrittenNumbers {
+//				if v == string(letter) {
+//					// take all the writtenNumbers that end with this letter
+//					for _, w := range writtenNumbers {
+//						wordLength := len(w)
+//
+//					}
+//				}
+//			}
+//		} else {
+//			// check if letter is in startWrittenNumbers
+//			for _, v := range startWrittenNumbers {
+//				if v == string(letter) {
+//					// take all the writtenNumbers that start with this letter
+//					// also take
+//				}
+//			}
+//		}
+//
+//	}
+//
+//	return false
+//}
 
 func isNumber(s uint8) (bool, int) {
 	i, err := strconv.Atoi(string(s))
