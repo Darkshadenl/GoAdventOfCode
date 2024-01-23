515ccb3beec286009d805157b9a6310cd5d0ba7e
Quinten M <quinten@infi.nl>
2024-01-23 17:28:25 +0100

    uniques

--- day_three/main.go
+++ day_three/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"bufio"
-	"io"
+	"fmt"
 	"log"
 	"os"
 )
@@ -30,7 +30,7 @@ func (q *Queue) GetElement(index int) (string, bool) {
 }
 
 func main() {
-	data, err := os.Open("./day_three/small_input.txt")
+	data, err := os.Open("./day_three/input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -39,7 +39,6 @@ func main() {
 	scanner := bufio.NewScanner(data)
 	scanner.Split(bufio.ScanLines)
 	lines := -1
-	currentLine := 0
 
 	for scanner.Scan() {
 		lines++
@@ -49,19 +48,35 @@ func main() {
 	scanner = bufio.NewScanner(data)
 	scanner.Split(bufio.ScanLines)
 
+	currentLine := 0
 	q := Queue{}
 	for scanner.Scan() {
 		if currentLine >= 3 {
 			log.Printf("currentLine: %d", currentLine)
+			doAssignment(q)
 			q.Dequeue()
 		}
 		linedata := scanner.Text()
 		q.Enqueue(linedata)
-		//data = append(data, linedata)
 		currentLine++
 	}
 }
 
-func doAssignment(r io.Reader) {
+func doAssignment(lines Queue) {
+	//validSigns := []string{"/", "+", "@", "%", "=", "$", "#", "-", "*", "&"}
 
+	//lineOne, _ := lines.GetElement(0)
+	//lineTwo, _ := lines.GetElement(1)
+	lineThree, _ := lines.GetElement(2)
+	runes := []rune(lineThree)
+
+	for i, r := range runes {
+		index := i
+		character := string(r)
+		fmt.Println(index, character)
+	}
+
+	//log.Println(lineOne)
+	//log.Println(lineTwo)
+	//log.Println(lineThree)
 }
